d53c611e4b88a12fccdaab15453b7865e09aa280
Ansh-star-777 <sangeetasiwach04@gmail.com>
2026-06-27 19:45:36 +1200

    Add function to calculate product of tuple numbers

---
+++ tuple_product.py
@@ -0,0 +1,12 @@
+# Tuple of numbers
+numbers = (2, 3, 4, 5)
+
+# Variable to store the product
+product = 1
+
+# Multiply each number
+for num in numbers:
+    product = product * num
+
+# Display the result
+print("The product of the tuple is:", product)
