66644f42bf182edb75e0caf3ffe55521a5e8ad15
Ansh-star-777 <sangeetasiwach04@gmail.com>
2026-06-27 19:49:22 +1200

    Add example of symmetric difference between sets

---
+++ symitric.py
@@ -0,0 +1,11 @@
+# Two example sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Symmetric difference
+result = set1.symmetric_difference(set2)
+
+# Print results
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Symmetric Difference:", result)
