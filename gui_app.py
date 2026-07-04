2d2d6436ea1a8f7d643c06397d3b05be32b29b54
Ansh-star-777 <sangeetasiwach04@gmail.com>
2026-07-04 16:25:18 +1200

    Create gui_app.py

---
+++ gui_app.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+window = Tk()
+
+window.title("Length Converter App")
+
+window.geometry("400x400")
+
+label = Label(window, text="Length Converter", bg="lightblue")
+label.pack(pady=20)
+
+window.mainloop()
